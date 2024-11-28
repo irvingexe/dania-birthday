25d39f3d9d5be2ab972389f54f8b26064b80bdc8
Irving Mariscales <mariscales.irving@gmail.com>
2024-11-28 15:04:55 -0700

    Surprise animation

--- app/components/Cake.js
+++ app/components/Cake.js
@@ -1,10 +1,23 @@
 'use client';
 
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Confetti } from "../utils/confetti.min.js";
+import { ConfettiRandom } from "../utils/ConfettiRandom.min.js";
+import Image from "next/image.js";
+import mazapan from "@/assets/mazapan.png";
+import mazapan1 from "@/assets/mazapan1.png";
+import mazapan2 from "@/assets/mazapan2.png";
+import dania from "@/assets/dania.png";
+import bg from "@/assets/bg.jpg";
+import balloons from "@/assets/balloons2.webp"
 
 export default function Cake() {
-
+  const [outAnimation, setOutAnimation] = useState(false);
+  const [mazapanOpen, setMazapanOpen] = useState(false);
+  const [preAnim, setPreAnim] = useState(false);
+  const happyBirthday = useRef(null);
   const cake = useRef(null);
+  const fireworks = useRef(null);
   const [candleCount, setCandleCount] = useState(9);
   let candles = useMemo(() => [], []);
   let audioContext;
@@ -81,6 +94,7 @@ export default function Cake() {
     console.log("getUserMedia not supported on your browser!");
   }
 
+  /*
   useEffect(() => {
     cake.current.addEventListener("click", function (event) {
       const rect = cake.current.getBoundingClientRect();
@@ -90,6 +104,7 @@ export default function Cake() {
       addCandle(left, top);
     });
   }, [addCandle])
+  */
 
   useEffect(() => {
     addCandle(23.399993896484375, 8)
@@ -103,12 +118,104 @@ export default function Cake() {
     addCandle(230.39999389648438, 15)
   }, [addCandle])
 
+  useEffect(() => {
+    let confetti = new Confetti({
+      particleCount: 100,
+      spread: 70,
+    });
+    confetti.setupElement("cake");
+    confetti.setFade(false);
+    confetti.setSize(1.5);
+    confetti.setPower(50);
+  }, [])
+
+  useEffect(() => {
+    let confetti = new ConfettiRandom({
+      particleCount: 100,
+      spread: 70,
+    });
+    confetti.setupElement("fireworks");
+    confetti.setFade(true);
+    confetti.setSize(1.5);
+    confetti.setPower(50);
+  }, [])
+
+  useEffect(() => {
+    if (mazapanOpen) {
+      setInterval(() => {
+        fireworks.current.click();
+      }, 700);
+    }
+  }, [mazapanOpen])
+
+  useEffect(() => {
+    if (!candleCount) {
+      cake.current.click();
+      setTimeout(() => {
+        setPreAnim(true);
+      }, 1500);
+      setTimeout(() => {
+        setMazapanOpen(true);
+      }, 4000);
+      setTimeout(() => {
+        setOutAnimation(true);
+      }, 3700);
+    };
+  }, [candleCount])
+
+  useEffect(() => {
+    const text = "HAPPY BIRTHDAY!";
+    let delay = 200;
+
+    happyBirthday.current.innerHTML = text
+      .split("")
+      .map(letter => {
+        console.log(letter);
+        return `<span>` + letter + `</span>`;
+      })
+      .join("");
+
+    Array.from(happyBirthday.current.children).forEach((span, index) => {
+      setTimeout(() => {
+        span.classList.add("wavy");
+      }, index * 60 + delay);
+    });
+  }, [])
+
+
   return (
-    <div>
-      <div className="candle-count-display">
+    <div className="w-full h-full overflow-hidden">
+      <div id="fireworks" ref={fireworks} />
+      {/*<div className="candle-count-display">
         Candles on the Cake: <span id="candleCount">{candleCount}</span>
+      </div>*/}
+      <div className="w-full h-full">
+        <Image src={bg} alt="bg" className={`bg opacity-0 ${outAnimation ? "opacity-100" : ""}`} />
       </div>
-      <div className="cake" ref={cake}>
+      <div className={`surprise  ${preAnim ? "move" : ""} ${!candleCount ? "rotate" : ""}`}>
+        <div className="relative h-full">
+          <div className={`dania ${outAnimation ? "animate" : ""}`} >
+            <div className="relative h-full">
+              <div className={`balloons absolute ${outAnimation ? "animate" : ""}`} >
+                <Image src={balloons} alt="balloons" />
+                <h1 className="absolute happy-birthday flex justify-center" ref={happyBirthday} />
+              </div>
+              <Image src={dania} alt="Dania" className="absolute" />
+            </div>
+          </div>
+          <div className={`mazapan`} >
+            <div className="relative h-full">
+              <Image src={mazapan1} alt="mazapan" className={`mazapan1 ${mazapanOpen ? "open" : ""}`} />
+              <Image src={mazapan2} alt="mazapan" className={`mazapan2 ${mazapanOpen ? "open" : ""}`} />
+            </div>
+          </div>
+          <div >
+            <Image src={mazapan} alt="mazapan" className={`mazapan-mini1 ${outAnimation ? "animate" : ""}`} />
+            <Image src={mazapan} alt="mazapan" className={`mazapan-mini2 ${outAnimation ? "animate" : ""}`} />
+          </div>
+        </div>
+      </div>
+      <div id="cake" className="cake" ref={cake}>
         <div className="plate"></div>
         <div className="layer layer-bottom"></div>
         <div className="layer layer-middle"></div>
