9f4f78943f41ce21dcf6130c807ab4e1b833dbd0
Irving Mariscales <mariscales.irving@gmail.com>
2024-11-30 16:04:37 -0700

    IOS test

--- app/components/Cake.js
+++ app/components/Cake.js
@@ -242,7 +242,6 @@ export default function Cake() {
       <div className={`surprise  ${preAnim ? "move" : ""}`}>
         <div className="relative h-full">
           <div >
-            <Image src={mazapan} alt="mazapan" className={`mazapan-mini1 ${outAnimation ? "animate" : ""}`} />
             <Image src={mazapan} alt="mazapan" className={`mazapan-mini2 ${outAnimation ? "animate" : ""}`} />
             <Image src={mazapan} alt="mazapan" className={`mazapan-mini3 ${outAnimation ? "animate" : ""}`} />
           </div>
